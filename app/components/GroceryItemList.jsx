8b06b1e7a9dba3e5d15905e14fc91e935439cfd0
thkprado <t.hkpra.do+github@gmail.com>
2015-08-28 04:52:47 -0300

    Init

--- app/components/GroceryItemList.jsx
+++ app/components/GroceryItemList.jsx
@@ -2,10 +2,23 @@ var React = require('react/addons');
 
 module.exports = React.createClass({
   render: function() {
+    /* jshint ignore:start */
+    /* beautify ignore:start */
     return (
       <div>
         <h1>Grocery Listify</h1>
+        <div>
+          {
+            this.props.items.map(function(item, index) {
+              return (
+                <div>{item.name}</div>
+              );
+            })
+          }
+        </div>
       </div>
     );
+    /* beautify ignore:end */
+    /* jshint ignore:end */
   }
 });
--- app/main.jsx
+++ app/main.jsx
@@ -4,4 +4,19 @@ console.log('Hello from JSX!');
 
 var GroceryItemList = require('./components/GroceryItemList.jsx');
 
-React.render(<GroceryItemList />, app);
+var initial = [{
+  name: 'Ice Cream'
+}, {
+  name: 'Waffles'
+}, {
+  name: 'Candy',
+  purchased: true
+}, {
+  name: 'Snarks'
+}];
+
+/* jshint ignore:start */
+/* beautify ignore:start */
+React.render(<GroceryItemList items={initial}/>, app);
+/* beautify ignore:end */
+/* jshint ignore:end */
