9574f229988e326e37e2035042fa62c1d4f76d3d
thkprado <t.hkpra.do+github@gmail.com>
2015-08-28 03:36:50 -0300

    Init

---
+++ app/main.jsx
@@ -0,0 +1,7 @@
+var React = require('react/addons');
+
+console.log('Hello from JSX!');
+
+var GroceryItemList = require('./components/GroceryItemList.jsx');
+
+React.render(<GroceryItemList />, app);
--- server/main.js
+++ server/main.js
@@ -1,8 +1,12 @@
 var express = require('express');
-
 var app = new express();
+var rootPath = require('path').normalize(__dirname + '/../');
 
-app.get('/', function(req, res) {
-  res.render('./../index.ejs', {});
-})
+app.set('views', rootPath + 'app');
+
+app
+  .get('/', function(req, res) {
+    res.render('index.ejs', {});
+  })
+  .use(express.static(rootPath + '.tmp'))
   .listen(7777);
